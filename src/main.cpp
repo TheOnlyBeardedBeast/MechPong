17da9fa6f867209f1fef3ba8bd9e55f344355d2c
Chronos <55414686+TheOnlyBeardedBeast@users.noreply.github.com>
2025-03-06 18:55:20 +0100

    Disabled unused code

--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,53 @@
 
 Paddle paddle0;
 Paddle paddle1;
-PongStepper y(21,20);
+// PongStepper y(21,20);
 
 
 void setup() {
   paddle0.initializeStepper(4,5);
   paddle0.initializeEncoder(0,1);
 
-  paddle1.initializeStepper(10,11);
-  paddle1.initializeEncoder(14,15);
+  paddle1.initializeStepper(14,15);
+  paddle1.initializeEncoder(19,20);
 
   Paddle::instances[0] = &paddle0;
   Paddle::instances[1] = &paddle1;
 
-  y.setMaxSpeed(1200);
-  y.setAcceleration(9600);
+  // y.setMaxSpeed(1200);
+  // y.setAcceleration(9600);
 
   Paddle::attachPaddles();
-  paddle0.subscribe(&y);
+  // paddle0.subscribe(&y);
 
   pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,HIGH);
+  // paddle0._stepper->moveTo(200);
 
   delay(1000);
 }
 
 void setup1()
 {
+  
   delay(2000);
 }
 
 void loop() {
+  // if(paddle0._stepper->isRunning()){
+  //   digitalWrite(LED_BUILTIN,HIGH);
+  // } else {
+  //   digitalWrite(LED_BUILTIN,LOW);
+  // }
+  delay(10000);
+  paddle0._stepper->moveTo(500);
+  delay(10000);
+  paddle0._stepper->moveTo(0);
+
 }
 
 void loop1() {
-  Paddle::update();
+  // Paddle::update();
   Paddle::run();
-  y.run();
+  // y.run();
 }
